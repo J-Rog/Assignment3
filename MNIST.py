2ecd66c9459098a9c491e1d4d7e362d11e4bc268
Jonathan Rogers <jonathanrogers@protonmail.com>
2021-04-26 23:55:53 -0500

    This would have been embarassing to forget

--- MNIST.py
+++ MNIST.py
@@ -8,29 +8,33 @@ from keras.datasets import mnist
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
 
+
 # This neural network class has a single hidden layer
 class NeuralNetwork:
     def __init__(self, shape):
         self.input = np.zeros(shape[0])
-        self.input_bias = np.random.uniform(-1,1,shape[0])
-        self.input_weights = np.random.uniform(-1,1,(shape[0],shape[1]))
+        self.input_bias = np.random.rand(shape[0])
+        self.input_weights = np.random.rand(shape[0],shape[1])
 
         self.hidden_layer = np.zeros(shape[1])
-        self.hidden_layer_bias = np.random.uniform(-1,1,shape[1])
-        self.hidden_layer_weights = np.random.uniform(-1,1,(shape[1],shape[2]))
+        self.hidden_layer_bias = np.random.rand(shape[1])
+        self.hidden_layer_weights = np.random.rand(shape[1],shape[2])
 
         self.output = np.zeros(shape[2])
-        self.output_bias = np.random.uniform(-1,1,shape[2])
+        self.output_bias = np.random.rand(shape[2])
 
         self.cost_function = 0
+        self.cost_function_values = np.zeros(len(self.output))
 
     def getInput(self, datum):
+        index = 0
         for i in range(0,datum.shape[0]):
             for j in range(0,datum.shape[1]):
-                self.input[i] = datum[i][j]
+                self.input[index] = datum[i][j]
+                index += 1
 
     def debug(self):
-        print(self.input.shape)
+        print(self.input)
 
     def feedforward(self):
 
@@ -53,14 +57,48 @@ class NeuralNetwork:
         for i in range(len(self.output)):
             if(i != answer):
                 total += self.output[i]**2
+                self.cost_function_values[i] = self.output[i]**2
             else:
                 total+= (self.output[i] - 1)**2
+                self.cost_function_values[i] = (self.output[i] - 1)**2
 
         self.cost_function = total
 
 
-    def backprop():
-        total = 0
+    def backprop(self):
+
+        hidden_biases = np.zeros(len(self.hidden_layer))
+
+        #Backprop for the weights between output and hidden layer
+        for j in range(0,len(self.output)):
+            for i in range(0,len(self.hidden_layer)):
+                    self.hidden_layer_weights[i][j] = sigmoid(self.cost_function_values[j]) * (1-sigmoid(self.cost_function_values[j]))
+
+
+        #Backprop for biases in hidden layer
+        for i in range(0,len(self.hidden_layer)):
+            for j in range(0,len(self.output)):
+                hidden_biases[i] = hidden_biases[i] + self.hidden_layer_weights[i][j]
+
+        for i in range(0,len(self.hidden_layer_bias)):
+            self.hidden_layer_bias[i] = hidden_biases[i]/28
+
+        for j in range(0,len(self.output)):
+                    self.output_bias[j] = sigmoid(self.cost_function_values[j]) * (1-sigmoid(self.cost_function_values[j]))
+
+
+
+        #Backprop for biases in output layer
+        for i in range(0,len(self.output_bias)):
+            self.output_bias[i] = random.random()
+
+        #Backprop for the weights between hidden layer and inputs
+        for j in range(0,len(self.hidden_layer)):
+            for i in range(0,len(self.input)):
+                self.input_weights[i][j] = math.tanh(self.input_weights[i][j] * \
+                                          self.hidden_layer_bias[j])
+
+
 
 
 
@@ -68,15 +106,32 @@ training_set, testing_set = mnist.load_data()
 X_train, y_train = training_set
 X_test, y_test = testing_set
 
-print(X_train[0].shape)
-print(y_train.shape)
-print((training_set[0].shape))
-
 #Create a neural network with 784 inputs, 10 outputs, and a hidden layer with 16 neurons
 MNIST = NeuralNetwork((784,28,10))
-MNIST.debug()
+
 
 #Train the neural network
-for i in range(4):
+for i in range(60000):
     MNIST.getInput(X_train[i])
     MNIST.feedforward()
+    MNIST.setCostFunction(y_train[i])
+    MNIST.backprop()
+
+
+right = 0
+#Test the neural network
+for i in range(10000):
+    MNIST.getInput(X_test[i])
+    MNIST.feedforward()
+    MNIST.setCostFunction(y_train[i])
+    MNIST.backprop()
+
+    # print(MNIST.output)
+    # #print(MNIST.input)
+    # print(np.argmax(MNIST.output))
+    # print(y_test[i])
+
+    if(np.argmax(MNIST.output) == y_test[i]):
+        right += 1
+
+print(right/10000)
